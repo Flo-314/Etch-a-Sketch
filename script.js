ed3a67ac8ad103346da737315c2353d6a60ed25d
flo-314 <julianflo1916@gmail.com>
2021-09-25 12:18:20 -0300

    Añadí la pizarra y un par de funciones

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const gridContainer = document.querySelector(".grid-container");
 
-const gridContainer = document.querySelector(".grid-container")
+// function that creates the grid and allow draw
 function createGrid(gridSize) {
   for (let i = 0; i < gridSize * gridSize; i++) {
+    //create a div, add class "grid" and append him to the grid container
     const grid = document.createElement("div");
-    grid.classList.add("grid")
-    gridContainer.appendChild(grid)
-   /*  const gridMDown =function(){grid.classList.add("mouseDown");}
-    grid.addEventListener("mousedown", gridMDown) */
+    grid.classList.add("grid");
+    gridContainer.appendChild(grid);
+    // allows draw with a event listener
+    const mouseOver = function () {
+      this.classList.add("mouseOver");
+    };
+    grid.addEventListener("mouseover", mouseOver);
   }
+}
 
+function restartGrid(){
+    const getAllGrid = document.querySelectorAll(".grid")
 }
