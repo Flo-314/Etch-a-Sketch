cd4b69565fd6d768a626dc6acfb51eb8949fbc26
flo-314 <julianflo1916@gmail.com>
2021-09-26 13:31:15 -0300

    the button of resize the grid works. now

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const gridContainer = document.querySelector(".grid-container");
 
-
+//grid btn
+const gridBtn = document.querySelector(".gridButton");
+gridBtn.addEventListener("mousedown", RecreateGrid);
+//create a grid with a new size. the grid size is defined by a prompt, and clean the original grid.
+function RecreateGrid() {
+  var gridPromp = prompt("Define the grid-size in a range of 1-100 :)");
+  createGrid(gridPromp);
+  restartGrid();
+}
 //erase btn
-const eraseBtn = document.querySelector(".eraseButton")
-eraseBtn.addEventListener("mousedown", restartGrid)
-//erase all function
-function restartGrid(){
-  const getAllGrid = document.querySelectorAll(".grid")
-  getAllGrid.forEach(e => e.classList.remove("mouseOver")); }
-  
+const eraseBtn = document.querySelector(".eraseButton");
+eraseBtn.addEventListener("mousedown", restartGrid);
+//erase all the board removing the mouseOverClassList
+function restartGrid() {
+  const getAllGrid = document.querySelectorAll(".grid");
+  getAllGrid.forEach((e) => e.classList.remove("mouseOver"));
+}
 
 // function that creates the grid and allow draw
 function createGrid(gridSize) {
@@ -21,8 +29,12 @@ function createGrid(gridSize) {
     const mouseOver = function () {
       this.classList.add("mouseOver");
     };
+
     grid.addEventListener("mouseover", mouseOver);
+    //change the GridTemplate rows/columns depending of the gridSize
+    gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
+    gridContainer.style.gridTemplateRows = "repeat(" + gridSize + ", 1fr)";
   }
 }
-
-createGrid(16)
+//set default 16x16 grid
+createGrid(16);
